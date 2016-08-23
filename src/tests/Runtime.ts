a8657e6af92c3859f19667307d85a336546a591d
Alexander Trefz <alexander.trefz@gmail.com>
2016-08-23 17:31:27 +0200

    Add Code to Parser & Runtime tests to prevent constant recompilation

---
+++ src/tests/Runtime.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../typings/index.d.ts" />
+
+import { } from '../Interfaces'
+
+describe('Runtime', () => {
+	let assert = require('assert')
+
+	describe('something', () => {
+		assert.ok(true)
+	})
+})
