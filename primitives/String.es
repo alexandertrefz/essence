3a65e0936beb0e4a15776a45638ee13160136f98
Alexander Trefz <alexander.trefz@gmail.com>
2016-08-31 19:36:37 +0200

    Add String.contains(self Self, other String) -> Bool

--- primitives/String.es
+++ primitives/String.es
@@ -3,6 +3,10 @@ type String {
 		return @@String.join(self, other)
 	}
 
+	contains (self Self, other String) -> Bool {
+		return @@String.contains(self, other)
+	}
+
 	equals (self Self, other String) -> Bool {
 		return @@String.equals(self, other)
 	}
--- src/runtimePrimitives/String.ts
+++ src/runtimePrimitives/String.ts
@@ -8,6 +8,10 @@ export let equals = (self: IValueNode, other: IValueNode) => {
 	return generateValueNode('Bool', self.value === other.value, {})
 }
 
+export let contains = (self: IValueNode, other: IValueNode) => {
+	return generateValueNode('Bool', self.value.includes(other.value), {})
+}
+
 export let join = (self: IValueNode, other: IValueNode) => {
 	return generateValueNode('String', self.value + other.value, {})
 }
