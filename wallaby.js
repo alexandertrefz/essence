578b001bd9157c03ff148a9b21e416d35570d83f
Alexander Trefz <alexander.trefz@gmail.com>
2017-10-25 20:29:13 +0200

    Configure Coverage reports in wallaby

--- wallaby.js
+++ wallaby.js
@@ -5,12 +5,18 @@ module.exports = function (wallaby) {
 			'!src/tests/*.ts'
 		],
 
+		filesWithNoCoverageCalculated: ['src/parser/grammar.ts'],
+
 		tests: [
 			'src/tests/*.ts'
 		],
 
 		env: {
 			type: 'node'
+		},
+
+		hints: {
+			ignoreCoverage: /ignore coverage/
 		},
 
 		testFramework: 'jasmine'
