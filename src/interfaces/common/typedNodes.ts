054b10048e68a4f0d04dc8a13e58fc53afc7a8a4
Alexander Trefz <alexander.trefz@gmail.com>
2017-11-23 23:15:08 +0100

    Add multiple common interfaces
    - Type
    - TypedNode
    - TypedSimpleNode

---
+++ src/interfaces/common/typedNodes.ts
@@ -0,0 +1,241 @@
+import { Position, Type, TypeType, ArrayType } from "./index"
+
+export type Node = ExpressionNode | StatementNode
+
+/////////////////
+/* Expressions */
+/////////////////
+
+export type ExpressionNode =
+	| NativeFunctionInvocationNode
+	| MethodInvocationNode
+	| FunctionInvocationNode
+	| MethodLookupNode
+	| ValueNode
+	| LookupNode
+	| IdentifierNode
+	| SelfNode
+	| CombinationNode
+
+export interface NativeFunctionInvocationNode {
+	nodeType: "NativeFunctionInvocation"
+	name: IdentifierNode | LookupNode
+	arguments: Array<ArgumentNode>
+	position: Position
+	type: Type
+}
+
+export interface MethodInvocationNode {
+	nodeType: "MethodInvocation"
+	name: MethodLookupNode
+	arguments: Array<ArgumentNode>
+	position: Position
+	type: Type
+}
+
+export interface FunctionInvocationNode {
+	nodeType: "FunctionInvocation"
+	name: ExpressionNode
+	arguments: Array<ArgumentNode>
+	position: Position
+	type: Type
+}
+
+export interface MethodLookupNode {
+	nodeType: "MethodLookup"
+	base: ExpressionNode
+	member: IdentifierNode
+	position: Position
+	baseType: TypeType
+	type: Type
+}
+
+export type ValueNode =
+	| RecordValueNode
+	| StringValueNode
+	| NumberValueNode
+	| BooleanValueNode
+	| FunctionValueNode
+	| ArrayValueNode
+
+export type RecordValueNode = {
+	nodeType: "RecordValue"
+	declaredType: Type | null
+	type: Type
+	members: {
+		[key: string]: ExpressionNode
+	}
+	position: Position
+}
+
+export type StringValueNode = {
+	nodeType: "StringValue"
+	value: string
+	position: Position
+	type: Type
+}
+
+export type NumberValueNode = {
+	nodeType: "NumberValue"
+	value: string
+	position: Position
+	type: Type
+}
+
+export type BooleanValueNode = {
+	nodeType: "BooleanValue"
+	value: boolean
+	position: Position
+	type: Type
+}
+
+export type FunctionValueNode = {
+	nodeType: "FunctionValue"
+	value: FunctionDefinitionNode
+	position: Position
+	type: Type
+}
+
+export type ArrayValueNode = {
+	nodeType: "ArrayValue"
+	values: Array<ExpressionNode>
+	position: Position
+	type: ArrayType
+}
+
+export interface LookupNode {
+	nodeType: "Lookup"
+	base: ExpressionNode
+	member: IdentifierNode
+	position: Position
+	type: Type
+}
+
+export interface IdentifierNode {
+	nodeType: "Identifier"
+	content: string
+	position: Position
+	type: Type
+}
+
+export interface SelfNode {
+	nodeType: "Self"
+	position: Position
+	type: Type
+}
+
+export interface CombinationNode {
+	nodeType: "Combination"
+	lhs: ExpressionNode
+	rhs: ExpressionNode
+	position: Position
+	type: Type
+}
+
+////////////////
+/* Statements */
+////////////////
+
+export type StatementNode =
+	| ConstantDeclarationStatementNode
+	| VariableDeclarationStatementNode
+	| VariableAssignmentStatementNode
+	| TypeDefinitionStatementNode
+	| IfElseStatementNode
+	| IfStatementNode
+	| ReturnStatementNode
+	| FunctionStatementNode
+
+export interface ConstantDeclarationStatementNode {
+	nodeType: "ConstantDeclarationStatement"
+	name: IdentifierNode
+	value: ExpressionNode
+	position: Position
+	declaredType: Type | null
+	type: Type
+}
+
+export interface VariableDeclarationStatementNode {
+	nodeType: "VariableDeclarationStatement"
+	name: IdentifierNode
+	value: ExpressionNode
+	position: Position
+	declaredType: Type | null
+	type: Type
+}
+
+export interface VariableAssignmentStatementNode {
+	nodeType: "VariableAssignmentStatement"
+	name: IdentifierNode
+	value: ExpressionNode
+	position: Position
+}
+
+export interface TypeDefinitionStatementNode {
+	nodeType: "TypeDefinitionStatement"
+	name: IdentifierNode
+	properties: {
+		[key: string]: Type
+	}
+	methods: {
+		[key: string]: {
+			method: FunctionValueNode
+			isStatic: boolean
+		}
+	}
+	position: Position
+	type: Type
+}
+
+export interface IfElseStatementNode {
+	nodeType: "IfElseStatement"
+	condition: ExpressionNode
+	trueBody: Array<Node>
+	falseBody: Array<Node>
+	position: Position
+}
+
+export interface IfStatementNode {
+	nodeType: "IfStatement"
+	condition: ExpressionNode
+	body: Array<Node>
+	position: Position
+}
+
+export interface ReturnStatementNode {
+	nodeType: "ReturnStatement"
+	expression: ExpressionNode
+	position: Position
+}
+
+export interface FunctionStatementNode {
+	nodeType: "FunctionStatement"
+	name: IdentifierNode
+	value: FunctionDefinitionNode
+	position: Position
+	type: Type
+}
+
+/////////////
+/* Helpers */
+/////////////
+
+export interface ParameterNode {
+	nodeType: "Parameter"
+	externalName: IdentifierNode | null
+	internalName: IdentifierNode
+	position: Position
+}
+
+export interface FunctionDefinitionNode {
+	nodeType: "FunctionDefinition"
+	parameters: Array<ParameterNode>
+	body: Array<Node>
+}
+
+export interface ArgumentNode {
+	nodeType: "Argument"
+	name: string | null
+	value: ExpressionNode
+	type: Type
+}
---
+++ src/interfaces/common/typedSimpleNodes.ts
@@ -0,0 +1,173 @@
+import { Type, ArrayType } from "./index"
+
+export type Node = ExpressionNode | StatementNode
+
+/////////////////
+/* Expressions */
+/////////////////
+
+export type ExpressionNode =
+	| NativeFunctionInvocationNode
+	| FunctionInvocationNode
+	| ValueNode
+	| LookupNode
+	| IdentifierNode
+	| CombinationNode
+
+export interface NativeFunctionInvocationNode {
+	nodeType: "NativeFunctionInvocation"
+	name: IdentifierNode | LookupNode
+	arguments: Array<ArgumentNode>
+	type: Type
+}
+
+export interface FunctionInvocationNode {
+	nodeType: "FunctionInvocation"
+	name: ExpressionNode
+	arguments: Array<ArgumentNode>
+	type: Type
+}
+
+export type ValueNode =
+	| RecordValueNode
+	| StringValueNode
+	| NumberValueNode
+	| BooleanValueNode
+	| FunctionValueNode
+	| ArrayValueNode
+
+export type RecordValueNode = {
+	nodeType: "RecordValue"
+	type: Type
+	members: {
+		[key: string]: ExpressionNode
+	}
+}
+
+export type StringValueNode = {
+	nodeType: "StringValue"
+	value: string
+	type: Type
+}
+
+export type NumberValueNode = {
+	nodeType: "NumberValue"
+	value: string
+	type: Type
+}
+
+export type BooleanValueNode = {
+	nodeType: "BooleanValue"
+	value: boolean
+	type: Type
+}
+
+export type FunctionValueNode = {
+	nodeType: "FunctionValue"
+	value: FunctionDefinitionNode
+	type: Type
+}
+
+export type ArrayValueNode = {
+	nodeType: "ArrayValue"
+	values: Array<ExpressionNode>
+	type: ArrayType
+}
+
+export interface LookupNode {
+	nodeType: "Lookup"
+	base: ExpressionNode
+	member: IdentifierNode
+	type: Type
+}
+
+export interface IdentifierNode {
+	nodeType: "Identifier"
+	name: string
+	type: Type
+}
+
+export interface CombinationNode {
+	nodeType: "Combination"
+	lhs: ExpressionNode
+	rhs: ExpressionNode
+	type: Type
+}
+
+////////////////
+/* Statements */
+////////////////
+
+export type StatementNode =
+	| VariableDeclarationStatementNode
+	| VariableAssignmentStatementNode
+	| TypeDefinitionStatementNode
+	| ChoiceStatementNode
+	| ReturnStatementNode
+	| FunctionStatementNode
+
+export interface VariableDeclarationStatementNode {
+	nodeType: "VariableDeclarationStatement"
+	name: IdentifierNode
+	value: ExpressionNode
+	type: Type
+	isConstant: boolean
+}
+
+export interface VariableAssignmentStatementNode {
+	nodeType: "VariableAssignmentStatement"
+	name: IdentifierNode
+	value: ExpressionNode
+}
+
+export interface TypeDefinitionStatementNode {
+	nodeType: "TypeDefinitionStatement"
+	name: IdentifierNode
+	properties: {
+		[key: string]: Type
+	}
+	methods: {
+		[key: string]: { method: FunctionValueNode; isStatic: boolean }
+	}
+	type: Type
+}
+
+export interface ChoiceStatementNode {
+	nodeType: "ChoiceStatement"
+	condition: ExpressionNode
+	trueBody: Array<Node>
+	falseBody: Array<Node>
+}
+
+export interface ReturnStatementNode {
+	nodeType: "ReturnStatement"
+	expression: ExpressionNode
+}
+
+export interface FunctionStatementNode {
+	nodeType: "FunctionStatement"
+	name: IdentifierNode
+	value: FunctionDefinitionNode
+}
+
+/////////////
+/* Helpers */
+/////////////
+
+export interface FunctionDefinitionNode {
+	nodeType: "FunctionDefinition"
+	parameters: Array<ParameterNode>
+	body: Array<Node>
+}
+
+export interface ParameterNode {
+	nodeType: "Parameter"
+	externalName: IdentifierNode | null
+	internalName: IdentifierNode
+}
+
+export interface ArgumentNode {
+	nodeType: "Argument"
+	name: string | null
+	value: ExpressionNode
+}
