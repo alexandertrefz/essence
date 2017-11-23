cc76f3c944bf02f83e353b298a0e1c712a61154d
Alexander Trefz <alexander.trefz@gmail.com>
2017-11-23 23:16:07 +0100

    Stub Optimiser

---
+++ src/optimiser/index.ts
@@ -0,0 +1,6 @@
+import { common } from "../interfaces"
+
+// TODO: Implement Optimisation
+export const optimise = (nodes: Array<common.typedSimple.Node>): Array<common.typedSimple.Node> => {
+	return nodes
+}
