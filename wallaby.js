e053eaff6eced1b782a7ea900db75f9b4268b4d1
Alexander Trefz <alexander.trefz@gmail.com>
2017-11-30 01:01:31 +0100

    Update Wallaby Configuration to work with istanbul

--- wallaby.js
+++ wallaby.js
@@ -2,16 +2,10 @@ module.exports = function(wallaby) {
 	return {
 		files: ["src/**/*.ts", "!src/tests/*.ts"],
 
-		filesWithNoCoverageCalculated: ["src/parser/grammar.ts"],
-
 		tests: ["src/tests/*.ts"],
 
 		env: {
 			type: "node",
-		},
-
-		hints: {
-			ignoreCoverage: /ignore coverage/,
 		},
 
 		testFramework: "jasmine",
