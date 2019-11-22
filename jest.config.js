14fbc0483bde5f0582932fd9d1ca613190371f53
Alexander Trefz <alexander.trefz@gmail.com>
2019-11-23 00:07:58 +0100

    Switch from jasmine to jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	rootDir: "src",
+	collectCoverage: true,
+	coveragePathIgnorePatterns: ["src/parser/grammar.ts", "src/tests", "testFiles", "wallaby.js", "src/coverage"],
+	preset: "ts-jest",
+	testEnvironment: "node",
+}
