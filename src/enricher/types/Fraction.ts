d13f6afab2b0899277f8612cd93661c4eab7c83e
Alexander Trefz <alexander.trefz@gmail.com>
2022-06-13 12:22:46 +0200

    Add add, subtract, and multiply Methods to Fraction Type

--- src/enricher/types/Fraction.ts
+++ src/enricher/types/Fraction.ts
@@ -5,7 +5,67 @@ const type: common.TypeType = {
 	name: "Fraction",
 	definition: { type: "Primitive", primitive: "Fraction" },
 	methods: {
+		add: {
+			type: "OverloadedMethod",
+			overloads: [
+				{
+					parameterTypes: [
+						{ name: null, type: { type: "Primitive", primitive: "Fraction" } },
+						{ name: null, type: { type: "Primitive", primitive: "Fraction" } },
+					],
+					returnType: { type: "Primitive", primitive: "Fraction" },
+				},
+				{
+					parameterTypes: [
+						{ name: null, type: { type: "Primitive", primitive: "Fraction" } },
+						{ name: null, type: { type: "Primitive", primitive: "Integer" } },
+					],
+					returnType: { type: "Primitive", primitive: "Fraction" },
+				},
+			],
+		},
+
+		subtract: {
+			type: "OverloadedMethod",
+			overloads: [
+				{
+					parameterTypes: [
+						{ name: null, type: { type: "Primitive", primitive: "Fraction" } },
+						{ name: null, type: { type: "Primitive", primitive: "Fraction" } },
+					],
+					returnType: { type: "Primitive", primitive: "Fraction" },
+				},
+				{
+					parameterTypes: [
+						{ name: null, type: { type: "Primitive", primitive: "Fraction" } },
+						{ name: null, type: { type: "Primitive", primitive: "Integer" } },
+					],
+					returnType: { type: "Primitive", primitive: "Fraction" },
+				},
+			],
+		},
+
 		divide: {
+			type: "OverloadedMethod",
+			overloads: [
+				{
+					parameterTypes: [
+						{ name: null, type: { type: "Primitive", primitive: "Fraction" } },
+						{ name: null, type: { type: "Primitive", primitive: "Fraction" } },
+					],
+					returnType: { type: "Primitive", primitive: "Fraction" },
+				},
+				{
+					parameterTypes: [
+						{ name: null, type: { type: "Primitive", primitive: "Fraction" } },
+						{ name: null, type: { type: "Primitive", primitive: "Integer" } },
+					],
+					returnType: { type: "Primitive", primitive: "Fraction" },
+				},
+			],
+		},
+
+		multiply: {
 			type: "OverloadedMethod",
 			overloads: [
 				{
